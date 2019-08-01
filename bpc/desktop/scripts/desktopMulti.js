7eb895c3451948c57c13acd5320d423f6103216f
rpelletierrefocus <47399964+rpelletierrefocus@users.noreply.github.com>
2019-08-01 09:08:31 -0400

    Update desktopMulti.js

--- bpc/desktop/scripts/desktopMulti.js
+++ bpc/desktop/scripts/desktopMulti.js
@@ -1,7 +1,7 @@
 module.exports = async function(context, commands) {
-  await commands.measure.start('https://en.wikipedia.org/wiki/Main_Page');
-  await commands.measure.start('https://en.wikipedia.org/wiki/Barack_Obama');
+  await commands.measure.start('https://bangordailynews.com');
+  await commands.measure.start('https://bangordailynews.com/browse/arts-culture/');
   return commands.measure.start(
-    'https://en.wikipedia.org/wiki/Democratic_Party_(United_States)'
+    'https://bangordailynews.com/bdn-maine/?ref=well'
   );
 };
